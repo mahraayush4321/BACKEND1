2f464aecdce85cb744dc0a08381683745a9a8a3b
TrulyMahra <mahraais653@gmail.com>
2024-03-23 23:32:28 +0530

    added cors

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const dbHelper = require('./src/helpers/dbHelper');
 const loadRoutes = require('./src/Routes');
 const CONST = require('./src/helpers/constants');
-const path = require('path')
+const path = require('path');
+const cors = require('cors')
 const app = express();
 
+app.use(cors({
+    origin:'*'
+}))
 require('dotenv').config();
 
 app.use(express.json({limit: '30mb'}));
