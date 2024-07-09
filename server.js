97d66423ce812eb743d8631a2b364931a4bfa443
mahraayush4321 <tanishqmahra@gmail.com>
2024-07-09 18:42:01 +0530

    socket update

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
 const app = require('./app');
 const http = require('http');
 const { PORT } = require('./config');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+app.use(cors());
 
 const httpServer = http.createServer(app);
+
+const io = socketIo(httpServer, {
+    cors: {
+      origin: "https://sportstriviax.netlify.app",
+      methods: ["GET", "POST"]
+    }
+  });
+
+
+// const io = socketIo(httpServer);
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+  
+    socket.on('sendMessage', (message) => {
+      io.emit('receiveMessage', message);
+    });
+  
+    socket.on('disconnect', () => {
+      console.log('User disconnected');
+    });
+  });
 
 httpServer.listen(PORT, () => {
     console.log(`server is running at port ${PORT}`);
