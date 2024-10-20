c7b31546a775839340c39ab526cee3dbb80ac291
mahraayush4321 <tanishqmahra@gmail.com>
2024-10-20 17:46:33 +0530

    removed secret creds

--- src/helpers/S3.js
+++ src/helpers/S3.js
@@ -5,7 +5,13 @@ const path = require('path');
 
 class S3Service {
     constructor(){
-        this.s3 = new S3Client({ region: 'eu-north-1' });
+       this.s3 = new S3Client({
+        region: process.env.AWS_REGION,
+        credentials: {
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+         }
+       });
     }
 
     uploadFile = async (file) => {
